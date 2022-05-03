03382e7b453c7b135871a4f6f09838de1e71761b
a118skr <98032893+a118skr@users.noreply.github.com>
2022-05-03 13:39:31 +0000

    [Finish]3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,13 @@
 class ListsController < ApplicationController
   def new
+     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = List.new
   end
 
-  def index
+  def create
+    list = List.new(list_params)  # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list.save                     # 3. データをデータベースに保存するためのsaveメソッド実行
+    redirect_to '/top'            # 4. トップ画面へリダイレクト
   end
 
   def show
@@ -10,4 +15,11 @@ class ListsController < ApplicationController
 
   def edit
   end
+
+   private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+
 end
