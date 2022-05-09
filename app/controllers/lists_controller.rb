72d9039ea9d3a5fee5c134623b4d679fee8f0053
a118skr <98032893+a118skr@users.noreply.github.com>
2022-05-09 02:37:07 +0000

    [Finish]3-3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,17 +4,29 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+
+
   def create
     list = List.new(list_params)  # １.&2. データを受け取り新規登録するためのインスタンス作成
     list.save                     # 3. データをデータベースに保存するためのsaveメソッド実行
     redirect_to '/top'            # 4. トップ画面へリダイレクト
   end
 
+  def index
+    @lists = List.all  
+  end
+  
+
   def show
   end
+  
+  
 
   def edit
   end
+
+
+
 
    private
   # ストロングパラメータ
